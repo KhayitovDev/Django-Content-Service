517ac20c8cc7fec7e4d1713c33f22a475de91490
KhayitovKhayitboy <kkhayitov@webster.edu>
2024-01-20 11:48:38 +0500

    created models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Tag, Blog, News
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Blog)
+admin.site.register(News)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    title=models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.title
+    
+class Tag(models.Model):
+    title=models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.title
+    
+class Blog(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    category=models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    tag=models.ManyToManyField(Tag) # a blog can have several tags at a time
+    title=models.CharField(max_length=220)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class News(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    category=models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    tag=models.ManyToManyField(Tag) # a blog can have several tags at a time
+    title=models.CharField(max_length=220)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+
